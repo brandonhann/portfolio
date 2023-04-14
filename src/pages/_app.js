ae0bd8dadc85e2f69616c2f393d2bffcfea6a64e
Brandon Hann <bhann1999@gmail.com>
2023-04-14 17:15:42 -0400

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { useState, useEffect } from 'react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = `theme-${theme}`
+  }, [theme])
+
+  return (
+    <div className={`min-h-screen bg-${theme === 'light' ? 'light-bg' : 'dark-bg'} text-${theme === 'light' ? 'light-text' : 'dark-text'
+      }`}>
+      <Component {...pageProps} toggleTheme={toggleTheme} />
+    </div>
+  )
 }
+
+export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,12 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
+  variants: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
     },
   },
+  darkMode: 'class',
   plugins: [],
 }
