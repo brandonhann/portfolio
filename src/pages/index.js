23a7175659317548b8a1249bcae8d597805f0a41
Brandon Hann <bhann1999@gmail.com>
2023-04-19 13:23:52 -0400

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,55 +11,58 @@ export default function Home({ toggleTheme }) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Brandon Hann</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className="w-full flex items-center justify-between p-6">
-        <h1 className="text-2xl font-bold">Brandon Hann</h1>
+        <h1 className="text-xl md:text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-300 uppercase">Brandon Hann</h1>
         <button
-          className="py-1 px-4 border rounded-md focus:outline-none hover:bg-opacity-80"
+          className="py-1 px-4 border shadow-md rounded-md focus:outline-none hover:bg-opacity-80 text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-300 font-bold"
           onClick={handleToggle}
         >
           {isDark ? 'Light Theme' : 'Dark Theme'}
         </button>
       </header>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 text-center">
-        <h2 className="text-4xl font-bold mb-6">My Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-          {/* Add your project cards here */}
-          {[
-            { title: 'KnowTools', description: 'AI powered tools utilizing Open AI', githubLink: 'https://github.com/brandonhann/knowtools', link: 'https://know.tools/' },
-            { title: 'Georgian College Open Concert', description: 'My COMM 1016 final project is a single responsive webpage promoting and providing information about the Global Rhythms Open Concert Night at Georgian College, an event I created to showcase student talent, creativity, and cultural diversity.', githubLink: 'https://github.com/brandonhann/gc-open-concert-night', link: 'https://gc-concert.vercel.app/' },
-            { title: 'Sacred Geometry Generator', description: 'Generate random shapes.', githubLink: 'https://github.com/brandonhann/sacred-geometry-generator', link: 'https://sacred-geometry-generator.vercel.app/' },
-            { title: 'Project 4', description: 'A brief description of Project 4.', githubLink: 'https://github.com/brandonhann/', link: 'https://sacred-geometry-generator.vercel.app/' },
-          ].map((project, index) => (
-            <div
-              key={index}
-              className="card text-left shadow-md rounded-lg p-6 flex flex-col"
-            >
-              <div className='flex align-middle gap-2 mb-4'>
-                <h3 className="text-xl font-semibold">{project.title}</h3>
-                <a
-                  href={project.githubLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="fa text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 transition-colors duration-200"
-                >
-                  <FaGithub size={24} />
-                </a>
+      <span className='bg-gradient-to-r from-cyan-500 to-blue-300 w-full h-1 mb-4 md:mb-0 flex justify-center items-center shadow-md'></span>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 text-center">
+        <section className='px-4 '>
+          <h3 className="text-2xl md:text-3xl font-bold mb-6 ">Showcase Projects</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mx-0 md:mx-10">
+            {[
+              { title: 'KnowTools', description: 'AI powered tools utilizing Open AI', githubLink: 'https://github.com/brandonhann/knowtools', link: 'https://know.tools/' },
+              { title: 'Georgian College Open Concert', description: 'My COMM 1016 final project is a single responsive webpage promoting and providing information about the Global Rhythms Open Concert Night at Georgian College, an event I created to showcase student talent, creativity, and cultural diversity.', githubLink: 'https://github.com/brandonhann/gc-open-concert-night', link: 'https://gc-concert.vercel.app/' },
+              { title: 'Sacred Geometry Generator', description: 'Generate random shapes.', githubLink: 'https://github.com/brandonhann/sacred-geometry-generator', link: 'https://sacred-geometry-generator.vercel.app/' },
+              { title: 'Project 4', description: 'A brief description of Project 4.', githubLink: 'https://github.com/brandonhann/', link: 'https://sacred-geometry-generator.vercel.app/' },
+            ].map((project, index) => (
+              <div
+                key={index}
+                className="card text-left shadow-md rounded-lg p-6 flex flex-col"
+              >
+                <div className='flex align-middle gap-2 mb-4'>
+                  <h4 className="text-xl font-semibold">{project.title}</h4>
+                  <a
+                    href={project.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fa text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 transition-colors duration-200"
+                  >
+                    <FaGithub size={24} />
+                  </a>
+                </div>
+                <p><a className='font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-300 hover:underline' href={project.link} target='_blank'>View app</a></p>
+                <p className="break-words">{project.description}</p>
               </div>
-              <a className='text-blue-400 hover:underline' href={project.link} target='_blank'>Try app</a>
-              <p className="break-words">{project.description}</p>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </section>
       </main>
 
-      <footer className="border-gray-300 dark:border-gray-800 mt-8 w-full h-12 flex justify-center items-center border-t">
+      <footer className="border-gray-300 dark:border-gray-800 mt-4 md:mt-0 w-full h-12 flex justify-center items-center border-t">
         <p className="text-sm">&copy; {new Date().getFullYear()} - Brandon Hann</p>
       </footer>
     </div>
