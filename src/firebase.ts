48afdbd7a31cd853947d25abc0dee8f6e19718c9
brandon <bhann1999@gmail.com>
2023-07-16 19:50:24 -0400

    env new fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,18 @@
-import './config';
 import { initializeApp, getApps } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 
 console.log(firebaseConfig);
 
-// Check if there are no initialized apps before initializing Firebase
 if (!getApps().length) {
     initializeApp(firebaseConfig);
 }
